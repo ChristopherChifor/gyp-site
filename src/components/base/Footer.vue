f3b596b1b8bd9d7315683d75c99afdf3eccee0e3
ChristopherChifor <christopher_chifor@yahoo.ca>
2022-05-19 14:28:12 -0400

    footer

--- src/components/base/Footer.vue
+++ src/components/base/Footer.vue
@@ -1,15 +1,15 @@
 <template>
-  <footer class="bg-white" aria-labelledby="footer-heading">
+  <footer class="bg-accent" aria-labelledby="footer-heading">
     <h2 id="footer-heading" class="sr-only">Footer</h2>
     <div class="max-w-7xl mx-auto py-12 pb-8 px-4 sm:px-6 lg:px-8">
-      <div class="grid md:grid-cols-2 gap-8">
+      <div class="grid md:grid-cols-3 gap-8">
         <router-link :to="{ name: 'home' }">
-          <img class="h-9 w-auto" src="@/assets/logo.svg" alt="Company Name" />
+          <img class="h-16 w-auto" src="@/assets/icons/logo-white.svg" alt="Company Name" />
         </router-link>
-        <div class="grid sm:grid-cols-2 gap-8">
+        <div class="grid sm:grid-cols-3 lg:gap-64 sm: gap-32">
           <div>
-            <h3 class="mb-3 text-sm font-semibold uppercase tracking-wider text-gray-400">Navigation</h3>
-            <div class="space-y-1">
+            <h3 class="mb-3 text-sm font-semibold uppercase tracking-wider text-white">Navigation</h3>
+            <div class="space-y-2">
               <router-link
                 v-for="route in navigation"
                 :key="route.label"
@@ -18,8 +18,8 @@
                   with-underline
                   w-max
                   transition
-                  hover:text-accent
-                  capitalize
+                  text-white
+                  uppercase
                 "
                 :to="route.path"
                 >{{ route.label }}</router-link
@@ -27,24 +27,28 @@
             </div>
           </div>
           <div>
-            <h3 class="mb-3 text-sm font-semibold uppercase tracking-wider text-gray-400">Contact Us</h3>
-            <div class="space-y-4">
-              <address class="not-italic">
-                Street <br />
-                City, Province POSTAL CODE
-              </address>
-              <a
-                href="tel:+19056379255"
-                class="block with-underline w-max hover:text-accent"
-                >Phone: xxx-xxx-xxxx
-              </a>
-              <a
-                href="mailto:info@teccweb.com"
-                class="block with-underline w-max hover:text-accent"
-                >Email: email@email.com</a
-              >
+            <h3 class="mb-3 text-sm font-semibold uppercase tracking-wider text-white">Contact Us</h3>
+            <div class="space-y-2">
+              <router-link
+          class="text-sm max-w-sm text-white flex items-center"
+          :to="{ name: home }"
+        >
+          CONTACT GYPTECH
+        </router-link>
+        <router-link
+          class="text-sm max-w-sm text-white flex items-center"
+          :to="{ name: home }"
+        >
+          CAREERS
+        </router-link>
             </div>
           </div>
+        <div class="mb-3 text-sm font-semibold uppercase tracking-wider text-white">Connect</div>
+        <div class="grid grid-cols-3">
+        <div class="h-8 w-8 bg-white"> icon</div>
+        <div class="h-8 w-8 bg-white">icon</div>
+        <div class="h-8 w-8 bg-white">icon</div>
+        </div>
         </div>
       </div>
       <div
@@ -56,7 +60,7 @@
         "
       >
         <p class="text-base text-gray-400">
-          &copy; Copyright 2021 Company Name
+           Gyptech &copy; 2022 | All rights reserved.
         </p>
       </div>
     </div>
